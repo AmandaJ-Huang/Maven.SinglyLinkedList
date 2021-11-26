d1bd578406df5bfcf79068c98e19d3f8376faef2
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-26 15:02:25 -0500

    getData, remove, contains, find

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -21,10 +21,15 @@ public class SinglyLinkedList<Element> {
         Node getNextNode() {
             return this.next;
         }
+
+        Element getData() {
+            return this.data;
+        }
     }
 
     private Node head = null;
     private Node tail = null;
+    private Integer size = 0;
 
     public void add(Element data) {
         Node node = new Node(data);
@@ -35,26 +40,87 @@ public class SinglyLinkedList<Element> {
             this.tail.next = node;
         }
         this.tail = node;
+        this.size++;
     }
 
     public void remove(int index) {
+        Node prevNode = this.head;
+        Node currNode = this.head.getNextNode();
 
+        if (index < 0 || index > this.size-1) {
+            return;
+        }
+        if (index == 0) {
+            this.head = this.head.getNextNode();
+        }
+
+        for (int i = 0; i < index-1; i++) {
+            currNode = currNode.getNextNode();
+            prevNode = prevNode.getNextNode();
+        }
+        prevNode.next = currNode.getNextNode();
+
+        this.size--;
     }
 
     public Boolean contains(Element element) {
-        return true;
+        Node currNode = this.head;
+//        for (int i = 0; i < size; i++) {
+//            if (currNode.getData().equals(element)) {
+//                return true;
+//            }
+//            currNode = currNode.getNextNode();
+//        }
+        while(currNode != null) {
+            if (currNode.getData().equals(element)) {
+                return true;
+            }
+            currNode = currNode.getNextNode();
+        }
+        return false;
     }
 
     public Integer find(Element element) {
+        Node currNode = this.head;
+        Integer counter = 0;
+
+        while(currNode != null) {
+            if (currNode.getData().equals(element)) {
+                return counter;
+            }
+            currNode = currNode.getNextNode();
+            counter++;
+        }
         return -1;
     }
 
-    public Integer size(Element element) {
-        return -1;
+    public Integer size() {
+        return this.size;
     }
 
     public Element get(int index) {
+
         return null;
+    }
+
+    public SinglyLinkedList<Element> copy() {
+        return null;
+    }
+
+    public void sort() {
+
+    }
+
+    public String toString() {
+        Node currNode = this.head;
+        String s = "";
+
+        while (currNode != null) {
+            s += currNode.data;
+            currNode = currNode.getNextNode();
+        }
+
+        return s;
     }
 
 
